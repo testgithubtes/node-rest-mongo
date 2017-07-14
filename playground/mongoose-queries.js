188180db8108b3cb301560bec1208dad301cfb8f
Mon Nom <so4raycpw@moakt.co>
2017-07-14 11:03:04 +0100

    make code more readable

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,7 +1,8 @@
-const {ObjectID} = require('mongodb');
-const {mongoose} = require('./../server/db/mongoose');
-const {Todos} = require('./../server/models/todos');
-const {User} = require('./../server/models/users');
+const { ObjectID } = require('mongodb');
+const { mongoose } = require('./../server/db/mongoose');
+const { Todos } = require('./../server/models/todos');
+const { User } = require('./../server/models/users');
+
 // var id = '596751d59a17fe24f2b97408';
 //
 // if(!ObjectID.isValid(id)){
@@ -31,12 +32,11 @@ const {User} = require('./../server/models/users');
 // }).catch((err) =>{
 //   console.log('they are ',err);
 // })
-var id ='596544535ab2ea130731487e'
-User.findById(id).then((user) =>{
-  if(!user){
+var id = '596544535ab2ea130731487e';
+User.findById(id).then((user) => {
+  if (!user) {
     throw new Error('Id of user not found');
-  }
-  console.log('user by id ',user);
-}).catch((err) =>{
-  console.log('Problem',err);
-})
+  }console.log('user by id ', user);
+}).catch((err) => {
+  console.log('Problem', err);
+});
--- server/server.js
+++ server/server.js
@@ -1,56 +1,50 @@
-var express = require ('express');
+var express = require('express');
 var bodyParser = require('body-parser');
 
-
-var {mongoose} = require('./db/mongoose');
-var {Todos} = require ('./models/todos');
-var {User} = require ('./models/users')
+var { mongoose } = require('./db/mongoose');
+var { Todos } = require('./models/todos');
+var { User } = require('./models/users');
 
 var app = express();
 
-
-
-
 app.use(bodyParser.json());
 
-
 // Route post todos
-app.post('/todos',(req,res) =>{
+app.post('/todos', (req, res) => {
   var todo = new Todos({
-    text : req.body.text,
-    completed :req.body.completed
-  })
-  todo.save().then((doc) =>{
+    text: req.body.text,
+    completed: req.body.completed,
+  });
+  todo.save().then((doc) => {
     res.send(doc);
-  },(e) =>{
-    res.status(400).send(e);
-  })
-});
-
-// Route post user
-app.post('/user',(req,res) => {
-  var user = new User({
-    email : req.body.email
-  })
-  user.save().then((doc) =>{
-    res.json(doc)
-  },(err) =>{
-    res.status(400).send(err);
-  })
-})
-
-
-// Route get todos
-app.get('/todos',(req,res) => {
-  Todos.find().then((Todos) =>{
-    res.json({Todos})
-  },(e) => {
+  }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.listen(3000,() => {
+// Route post user
+app.post('/user', (req, res) => {
+  var user = new User({
+    email: req.body.email,
+  });
+  user.save().then((doc) => {
+    res.json(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+// Route get todos
+app.get('/todos', (req, res) => {
+  Todos.find().then((Todos) => {
+    res.json({ Todos });
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
   console.log('start server on port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
